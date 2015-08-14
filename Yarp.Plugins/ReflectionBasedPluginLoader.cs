2921720c056df1e6ec73a19652d5409ba4d33371
joakimskoog <joakimskog@mail.nu>
2015-08-14 18:19:09 +0200

    Started to work on the plugin loader. Created a settings class that will hold information that will be useful for the reflection based plugin loader.

--- Yarp.Plugins/ReflectionBasedPluginLoader.cs
+++ Yarp.Plugins/ReflectionBasedPluginLoader.cs
@@ -1,10 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Yarp.Plugins
 {
     public class ReflectionBasedPluginLoader : IPluginLoader
     {
+        private readonly ReflectionBasedPluginLoaderSettings _settings;
+
+        public ReflectionBasedPluginLoader(ReflectionBasedPluginLoaderSettings settings)
+        {
+            if (settings == null) throw new ArgumentNullException("settings");
+            _settings = settings;
+        }
+
         public IEnumerable<YarpPluginContainer> LoadPlugins()
         {
             return Enumerable.Empty<YarpPluginContainer>();
---
+++ Yarp.Plugins/ReflectionBasedPluginLoaderSettings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Yarp.Plugins
+{
+    public class ReflectionBasedPluginLoaderSettings
+    {
+        /// <summary>
+        /// The path to the folder where the plugins reside
+        /// </summary>
+        public string PluginsPath { get; private set; }
+
+        /// <summary>
+        /// The search pattern that will be used to find plugins
+        /// </summary>
+        public string PluginFileSearchPattern { get; private set; }
+
+        /// <summary>
+        /// The search pattern that will beu sed to find metadata for plugins
+        /// </summary>
+        public string PluginMetadataSearchPattern { get; private set; }
+
+        /// <summary>
+        /// Constructs a new <see cref="ReflectionBasedPluginLoaderSettings"/> which can be used in the <seealso cref="ReflectionBasedPluginLoader"/>
+        /// </summary>
+        /// <param name="pluginsPath">The path to the folder where the plugins reside</param>
+        /// <param name="pluginFileSearchPattern">The search pattern that will be used to find plugins</param>
+        /// <param name="pluginMetadataSearchPattern">The search pattern that will beu sed to find metadata for plugins</param>
+        public ReflectionBasedPluginLoaderSettings(string pluginsPath, string pluginFileSearchPattern, string pluginMetadataSearchPattern)
+        {
+            if (string.IsNullOrEmpty(pluginsPath)) throw new ArgumentException("PluginsPath can't be empty", "pluginsPath");
+            if (string.IsNullOrEmpty(pluginFileSearchPattern)) throw new ArgumentException("PluginFileSearchPattern can't be empty", "pluginFileSearchPattern");
+            if (string.IsNullOrEmpty(pluginMetadataSearchPattern)) throw new ArgumentException("PluginMetadataSearchPattern can't be empty", "pluginMetadataSearchPattern");
+            PluginsPath = pluginsPath;
+            PluginFileSearchPattern = pluginFileSearchPattern;
+            PluginMetadataSearchPattern = pluginMetadataSearchPattern;
+        }
+    }
+}
